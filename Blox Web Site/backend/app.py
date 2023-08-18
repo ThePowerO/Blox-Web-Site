70b0b01684becd27f85865527d3df5b76024caf5
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-17 23:35:23 -0300

    Update app.py

--- Blox Web Site/backend/app.py
+++ Blox Web Site/backend/app.py
@@ -25,3 +25,4 @@ def create_acc():
         if password == confirm_password:
             db = get_db()
             cursor = db.cursor()
+            cursor.execute('INSERT INTO users (nickname, password) VALUES (?, ?)', (nickname, password))
