2000bba51b1dd61039b9680fa164d544a8b2070b
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-17 23:32:26 -0300

    Update app.py

--- Blox Web Site/backend/app.py
+++ Blox Web Site/backend/app.py
@@ -3,9 +3,4 @@ import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+DATABASE = 'users.db'
