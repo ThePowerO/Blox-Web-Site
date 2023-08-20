d5a3196597cfd6b08f18f4f6d595ed8b204decf5
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-19 23:39:29 -0300

    Update app.py

--- Blox Web Site/backend/app.py
+++ Blox Web Site/backend/app.py
@@ -11,3 +11,5 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+
+@app.route("/")
