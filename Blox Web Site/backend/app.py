ea16920ff5deef35f4c184d5de3194b6c1106114
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-19 23:39:20 -0300

    Update app.py

--- Blox Web Site/backend/app.py
+++ Blox Web Site/backend/app.py
@@ -10,3 +10,4 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
