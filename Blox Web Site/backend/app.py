c5c6df3b5e5c4dd0615b4265577af2458c8a2115
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-19 23:38:32 -0300

    Update app.py

--- Blox Web Site/backend/app.py
+++ Blox Web Site/backend/app.py
@@ -2,3 +2,4 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'vcx8vok1mk7csmvok2n3e4g43'
