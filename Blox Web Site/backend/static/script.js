49fcecc9d1acedb298ed8bffbd58062060c6d3e7
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-22 23:21:24 -0300

    Update script.js

--- Blox Web Site/backend/static/script.js
+++ Blox Web Site/backend/static/script.js
@@ -2,3 +2,4 @@ const mainOptions = document.querySelectorAll(".nav-link");
 
 const toggleActiveClass = (selectedOption) => {
     mainOptions.forEach(option => {
+        if (option === selectedOption) {
