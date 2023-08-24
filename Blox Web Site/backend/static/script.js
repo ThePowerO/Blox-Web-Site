ed967fb6e18213ffba195abbd5552bb387c15143
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-23 22:57:08 -0300

    Update script.js

--- Blox Web Site/backend/static/script.js
+++ Blox Web Site/backend/static/script.js
@@ -247,6 +247,7 @@ selectElements.forEach((selectElement, index) => {
             // Fighting Styles
             else if (selectedValue === "Godhuman") {
                 imageElement.src = "../static/img/Godhuman.png"; 
+            }
 
         });
 });
