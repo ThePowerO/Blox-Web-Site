84ed855eb5f77a8fe357829db9fb9b9a8ced1d71
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-08-26 23:51:50 -0300

    Update app.py

--- Blox Web Site/backend/app.py
+++ Blox Web Site/backend/app.py
@@ -89,6 +89,7 @@ def edit_combo(id):
         combo_to_edit.fruit = new_fruit
         combo_to_edit.fighting_style = new_fighting_style
         combo_to_edit.sword = new_sword
+        combo_to_edit.gun = new_gun
 
 
 if __name__ == '__main__':
