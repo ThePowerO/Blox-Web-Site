ae5091f4521ee48bbd1ad65cea0a19f6cd0d3d12
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-09-02 19:51:15 -0300

    Update languages.js

--- Blox Web Site/backend/static/languages.js
+++ Blox Web Site/backend/static/languages.js
@@ -4,6 +4,7 @@ $(".br").click(function () {
     $(".nav").children().eq(1).text("Dashboard");
     $(".nav").children().eq(2).text("Entrar");
     $(".nav").children().eq(3).text("Criar Conta");
+    // Things to Toggle
     $(".language-selected").text("pt-BR");
     $(".language-selected").removeClass("change-en change-fr change-kr");
     $(".language-selected").addClass("change-br");
@@ -17,6 +18,7 @@ $(".en").click(function () {
     $(".nav").children().eq(1).text("Dashboard");
     $(".nav").children().eq(2).text("Log In");
     $(".nav").children().eq(3).text("Create Account");
+    // Things to Toggle
     $(".language-selected").text("en-US");
     $(".language-selected").removeClass("change-br change-fr change-kr");
     $(".language-selected").addClass("change-en");
@@ -30,6 +32,7 @@ $(".fr").click(function () {
     $(".nav").children().eq(1).text("Dashboard");
     $(".nav").children().eq(2).text("Entrer");
     $(".nav").children().eq(3).text("Crée Compte");
+    // Things to Toggle
     $(".language-selected").text("FR");
     $(".language-selected").removeClass("change-br change-en change-kr");
     $(".language-selected").addClass("change-fr");
@@ -43,6 +46,7 @@ $(".kr").click(function () {
     $(".nav").children().eq(1).text("대시보드");
     $(".nav").children().eq(2).text("로그인");
     $(".nav").children().eq(3).text("계정 생성");
+    // Things to Toggle
     $(".language-selected").text("KR");
     $(".language-selected").removeClass("change-br change-en change-fr");
     $(".language-selected").addClass("change-kr");
