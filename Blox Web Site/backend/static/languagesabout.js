eb057ea5ad526f2dda70e19632051ced4320cf00
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-09-02 23:54:23 -0300

    Update languagesabout.js

--- Blox Web Site/backend/static/languagesabout.js
+++ Blox Web Site/backend/static/languagesabout.js
@@ -3,3 +3,4 @@ $(".br").click(function () {
     // Header
     $("#nav ul li:nth-child(1) a").html('<i class="bi bi-book" aria-hidden="true"></i> Sobre');
     $("#nav ul li:nth-child(2) a").html('<i class="bi bi-plus-square" aria-hidden="true"></i> Criar Combo');
+    $("#nav ul li:nth-child(3) a").html('<i class="bi bi-journals" aria-hidden="true"></i> Ver Combo');
